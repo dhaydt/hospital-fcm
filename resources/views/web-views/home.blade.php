3d489710b927058fc0a469625823084ceae28f1e
dhaydt <1992dayat@gmail.com>
2022-02-09 18:05:51 +0700

    alert

--- resources/views/web-views/home.blade.php
+++ resources/views/web-views/home.blade.php
@@ -83,10 +83,10 @@
                     },
                     dataType: 'JSON',
                     success: function (response) {
-                        console.log('Token saved successfully.');
+                        alert('Token saved successfully.');
                     },
                     error: function (err) {
-                        console.log('User Chat Token Error'+ err);
+                        alert('User Chat Token Error'+ err);
                     },
                 });
 
